9b8773d644ed52a2e8450a2ced90a1438113723f
roso1nik <103901662+roso1nik@users.noreply.github.com>
2022-09-17 20:41:30 +0300

    update v2.0

---
+++ precache-manifest.d09f23c9fe59bbb2cf1ba2964c3ea56f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fbd52ca2c7c061752be",
+    "url": "/artist-taplink/static/css/main.7b9a80ac.chunk.css"
+  },
+  {
+    "revision": "3fbd52ca2c7c061752be",
+    "url": "/artist-taplink/static/js/main.e7c28e5b.chunk.js"
+  },
+  {
+    "revision": "a68c4aa0d4d1082e098d",
+    "url": "/artist-taplink/static/js/runtime~main.a9171a72.js"
+  },
+  {
+    "revision": "b863af09fb801a71610e",
+    "url": "/artist-taplink/static/js/2.45ec37fe.chunk.js"
+  },
+  {
+    "revision": "ad398f5d15426a6b4af60b00f41865ff",
+    "url": "/artist-taplink/static/media/cover.ad398f5d.jpg"
+  },
+  {
+    "revision": "b9bdef50db2f2c9f581669d3843f87f5",
+    "url": "/artist-taplink/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/artist-cover/precache-manifest.d88ee230b6dcf1332efac2caebf78594.js"
+  "/artist-taplink/precache-manifest.d09f23c9fe59bbb2cf1ba2964c3ea56f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/artist-cover/index.html", {
+workbox.routing.registerNavigationRoute("/artist-taplink/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
